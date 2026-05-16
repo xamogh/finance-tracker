5c9c417ad43cb54f6c017832645cae6ff757ff9a
Amogh Rijal <xamoghx@gmail.com>
2026-05-16 21:26:03 +0545

    chore: add convex generated stubs

---
+++ src/convex/_generated/api.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ */
+
+import { anyApi } from 'convex/server';
+
+export const api = anyApi;
+export const internal = anyApi;
+
